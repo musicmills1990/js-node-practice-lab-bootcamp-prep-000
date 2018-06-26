1301bda44d56d809797b52853523a277b96db265
mdmills1990@gmail.com <mdmills1990@gmail.com>
2018-06-26 06:27:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // write your code below!
 
 var name = "Marc"
-var height = "74"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1)
+console.log(height+1)
+
 
 
 
